765a78bc53c3a4a2325a6b6056568507ec0695d0
TwuanMinn <twuanminn47@gmail.com>
2026-01-21 10:41:08 -0800

    chore: upgrade typescript to v5 to resolve i18next peer dependency conflict

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders status badge', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const statusElement = screen.getByText(/open for work/i);
+  expect(statusElement).toBeInTheDocument();
 });
